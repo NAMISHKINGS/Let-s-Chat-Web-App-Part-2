d14a52f5cb77999fa96da537960a3843be652963
NAMISHKINGS <119147797+NAMISHKINGS@users.noreply.github.com>
2023-05-27 21:25:28 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAZEgzGfeMeM4hEvV-ezPx23Nam6XOfGqQ",
+    authDomain: "let-s-chat-web-app-91005.firebaseapp.com",
+    databaseURL: "https://let-s-chat-web-app-91005-default-rtdb.firebaseio.com",
+    projectId: "let-s-chat-web-app-91005",
+    storageBucket: "let-s-chat-web-app-91005.appspot.com",
+    messagingSenderId: "756283020979",
+    appId: "1:756283020979:web:c7ae2b222e41d7fec182c6",
+    measurementId: "G-4D2MJ0N96N"
+  };
